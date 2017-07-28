78a81ec0e5157a58c5e4afd05af387df28d49623
Didrik Rokhaug <didrik.rokhaug@gmail.com>
2017-07-28 16:48:42 +0200

    Add test, tested OK on HW

--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
 // tests go here; this will not be compiled when this package is used as a library
+
+
+display.print("Dette er en lang tekst som\nmaa printes over mange linjer.")
+display.setXYPosition(20, 5)
+display.print("Mer tekst")
+display.setBacklight(1)
