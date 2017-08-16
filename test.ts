1b4d15d86cafc9c8348efe99a0c11d654c2efab8
Didrik Rokhaug <didrik.rokhaug@gmail.com>
2017-08-16 09:12:38 +0200

    Mkae package norwegian again

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@
 //display.changeBusPins(DigitalPin.P3, DigitalPin.P4, DigitalPin.P5)
 //display.changeSPIPins(DigitalPin.P6, DigitalPin.P7, DigitalPin.P16) // Does not work
 //display.changeLightPin(DigitalPin.P8)
-display.changePins(DigitalPin.P3, DigitalPin.P4, DigitalPin.P5, DigitalPin.P8)
+//display.changePins(DigitalPin.P3, DigitalPin.P4, DigitalPin.P5, DigitalPin.P8)
 display.print("Dette er en lang tekst som\nmaa printes over mange linjer.")
 display.setXYPosition(43, 4)
 display.print("Mer tekst")
